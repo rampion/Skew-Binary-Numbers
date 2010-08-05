4883369c4daa5a2977797bd8296d585405838c45
Noah Luck Easterly <noah.easterly@gmail.com>
2010-08-05 17:20:44 +0100

    trying out some stuff to make a full binary tree class

--- skew.hs
+++ skew.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE MultiParamTypeClasses,FlexibleInstances,UndecidableInstances #-}
 module Skew where
 
-type Coef = Int
-type Index = Int
-type Base = Int
-type Number = ( Base, [(Coef,Index)] )
+data Leaf a = Leaf a
+data Branch b a = Branch { left :: b a, value :: a, right :: b a }
+
+class FullBinaryTree t where
+  height :: t a -> Int
+  size :: t a -> Int
+
+instance FullBinaryTree Leaf where
+  height _ = 1
+  size _ = 1
+
+instance (FullBinaryTree t) => FullBinaryTree (Branch t) where
+  height t = 1 + (height $ left t)
+  size t = 1 + 2 * (size $ left $ t)
+
+
+data AscendingList a b = a :<=: b
+class IsSmallerThan a b
+instance (IsSmallerThan a b, IsSmallerThan b c) => IsSmallerThan a c
+instance IsSmallerThan a ()
+instance (FullBinaryTree t, IsSmallerThan (Branch t a) b) => IsSmallerThan (t a) (AscendingList (Branch t a) b)
